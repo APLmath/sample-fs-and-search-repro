c5cfbe23f825379ee4f13c3ad2349e459675613d
Andrew Lee <andlee@microsoft.com>
2020-05-10 18:54:44 -0700

    Index disk in constructor

--- src/SuperSimpleStringSystemProvider.ts
+++ src/SuperSimpleStringSystemProvider.ts
@@ -15,7 +15,6 @@ import {
   Progress,
   TextSearchResult,
   CancellationToken,
-  ProviderResult,
   TextSearchComplete,
   TextSearchProvider,
   Range,
@@ -25,27 +24,89 @@ import { TextEncoder } from 'util';
 
 /**
  * Extremely basic filesystem of text-only files and directories.
+ *
+ * For searching, this filesystem searches on whole words, where a file matches
+ * if it contains at least an instance of each word in the query. This is an
+ * extremely basic approximation of how web search engines or code search
+ * engines behave. Here, the expectation is that each word that was matched
+ * should be highlighted.
+ *
+ * For example, say we have two files with their respective content:
+ * - file1: I say Hello world!
+ * - file2: To the world, hello to you!
+ *
+ * Here are some sample search queries, and what the results should be. The
+ * highlighted portions are in brackets:
+ * - "hello"
+ *     file1:
+ *        I say [Hello] world!
+ *     file2:
+ *        To the world, [hello] to you!
+ * - "hello world"
+ *     file1:
+ *        I say [Hello] world!
+ *        I say Hello [world]!
+ *     file2:
+ *        To the [world], hello to you!
+ *        To the world, [hello] to you!
+ *   "hello wor"
+ *     no matches, because neither file has "wor" by itself
+ *   "hello to"
+ *     file2:
+ *        [To] the world, hello to you!
+ *        To the [world], hello to you!
+ *        To the world, hello [to] you!
+ *     file1 doesn't match because it doesn't have "to"
  */
 export interface SampleDirectory {
   [filename: string]: SampleFile; 
 }
 type SampleFile = SampleDirectory | string;
 
+interface WholeWordSearchMatchRegion {
+  start: number;
+  end: number
+}
+
+interface WholeWordSearchMatch {
+  [fileUriStr: string]: WholeWordSearchMatchRegion[];
+}
+
+interface WholeWordSearchIndex {
+  [word: string]: WholeWordSearchMatch;
+}
+
 export class SuperSimpleStringSystemProvider implements FileSystemProvider, TextSearchProvider {
 
-  private _fileUris: Uri[] = [];
+  private _index: WholeWordSearchIndex = {};
 
   constructor(private _disk: SampleDirectory) {
-    function helper(file: SampleFile, uriStr: string, fileUris: Uri[]) {
+    // Index the disk contents.
+    let regex = RegExp('\\w+', 'g');
+    function helper(file: SampleFile, uriStr: string, index: WholeWordSearchIndex) {
       if (typeof file === 'string') {
-        fileUris.push(Uri.parse(uriStr));
+        for(const match of file.matchAll(regex)) {
+          const word = match[0].toLowerCase();
+          const start = match.index!;
+          const end = start + word.length;
+          if (!index.hasOwnProperty(word)) {
+            index[word] = {};
+          }
+          if (!index[word].hasOwnProperty(uriStr)) {
+            index[word][uriStr] = [];
+          }
+          index[word][uriStr].push({
+            start: start,
+            end: end
+          });
+        }
       } else {
         Object.keys(file).forEach((filename) => {
-          helper(file[filename], uriStr + '/' + filename, fileUris);
+          helper(file[filename], uriStr + '/' + filename, index);
         });
       }
     }
-    helper(this._disk, 'samplefs://', this._fileUris);
+    helper(this._disk, 'samplefs://', this._index);
   }
 
   /**
@@ -150,35 +211,43 @@ export class SuperSimpleStringSystemProvider implements FileSystemProvider, Text
    */
 
   async provideTextSearchResults(query: TextSearchQuery, options: TextSearchOptions, progress: Progress<TextSearchResult>, token: CancellationToken): Promise<TextSearchComplete> {
-    let keywords = query.pattern.split(/\s+/);
-    let regexes = keywords.map((keyword) => RegExp('\\b' + keyword + '\\b', 'g'));
-    let promises = this._fileUris.map(async (uri) => {
-      let textDoc = await workspace.openTextDocument(uri);
-      let content = textDoc.getText();
-      console.log(uri);
-      regexes.forEach((regex) => {
-        console.log(regex.source);
-        let match:RegExpExecArray | null = null;
-        while((match = regex.exec(content)) != null) {
-          console.log(match);
-          console.log(match.index);
-          console.log(match[0].length);
-          let start = textDoc.positionAt(match.index);
-          let end = textDoc.positionAt(match.index + match[0].length);
-          let range = new Range(start, end);
-          console.log(textDoc.getText(range));
-          let line = textDoc.lineAt(start.line);
-          let pmatch = new Range(0, start.character, 0, end.character);
-          progress.report({
-            uri: uri,
-            ranges: range,
-            preview: {
-              text: line.text,
-              matches: pmatch
-            }
-          });
-        }
-      });
+    let words = query.pattern.trim().split(/\s+/);
+
+    let results: WholeWordSearchMatch = {};
+    let firstWord = true;
+    for (let word of words) {
+      word = word.toLowerCase();
+      let wordResults = this._index[word] || {};
+      if (firstWord) {
+        results = wordResults;
+      } else {
+        let newResults: WholeWordSearchMatch = {};
+        Object.keys(results).forEach((uriStr) => {
+          if (wordResults.hasOwnProperty(uriStr)) {
+            newResults[uriStr] = results[uriStr].concat(wordResults[uriStr]);
+          }
+        });
+        results = newResults;
+      }
+      firstWord = false;
+    }
+
+    let promises = Object.keys(results).map(async (uriStr) => {
+      const uri = Uri.parse(uriStr);
+
+      const textDoc = await workspace.openTextDocument(uri);
+      for (const matchRegion of results[uriStr]) {
+        let start = textDoc.positionAt(matchRegion.start);
+        let end = textDoc.positionAt(matchRegion.end);
+        progress.report({
+          uri: uri,
+          ranges: new Range(start, end),
+          preview: {
+            text: textDoc.lineAt(start.line).text,
+            matches: new Range(0, start.character, 0, end.character)
+          }
+        });
+      }
     });
     await Promise.all(promises);
     return {
